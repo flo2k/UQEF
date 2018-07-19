6338c18f793844055654b0ffc520f6580d6eafa0
Florian Künzner <florian.kuenzner@tum.de>
2018-07-19 08:17:20 +0200

    setup.py support for python3: dependend install requirements

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ setup(
     version="0.2",
     url='https://gitlab.lrz.de/di73wal/UQEF',
     author="Florian Kuenzner",
-    author_email="florian.kuenzner@tum.de",
+    author_email='florian.kuenzner@tum.de',
     license='MIT',
     platforms='any',
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=[
-        "argparse", "chaospy", "dill", "enum", "joblib", "matplotlib", "more_itertools", "mpi4py", "multiprocessing", "numpy", "scipy", "setuptools", "tabulate"
+        'argparse', 'chaospy', 'dill', 'joblib', 'matplotlib', 'more_itertools', 'mpi4py', 'numpy', 'scipy', 'setuptools', 'tabulate',
+        'enum;python_version<"3.0"', 'multiprocessing;python_version<"3.0"'
     ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
