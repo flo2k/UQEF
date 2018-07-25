e472c10fccce79724037c1f8241f239ee8bbfd3b
Florian Künzner <florian.kuenzner@tum.de>
2018-07-25 13:13:29 +0200

    default: Linear Solver - SC

--- examples/start_simple_example_testmodel.sh
+++ examples/start_simple_example_testmodel.sh
@@ -7,10 +7,10 @@ python_cmd=python
 #python_cmd=python3
 
 # Linear Solver - SC
-#$python_cmd simple_example_testmodel.py \
-#                                    --model "testmodel" \
-#                                    --uq_method "sc" --sc_q_order 2 --sc_p_order 2 \
-#                                    --uncertain "all"
+$python_cmd simple_example_testmodel.py \
+                                    --model "testmodel" \
+                                    --uq_method "sc" --sc_q_order 2 --sc_p_order 2 \
+                                    --uncertain "all"
 
 # Linear Solver - MC
 #$python_cmd simple_example_testmodel.py \
@@ -36,11 +36,11 @@ python_cmd=python
 
 
 # MpiPoolSolver - SC
-mpiexec -n 4 $python_cmd simple_example_testmodel.py \
-                                    --model "testmodel" \
-                                    --uq_method "sc" --sc_q_order 3 --sc_p_order 2 \
-                                    --uncertain "all" \
-                                    --mpi
+#mpiexec -n 4 $python_cmd simple_example_testmodel.py \
+#                                    --model "testmodel" \
+#                                    --uq_method "sc" --sc_q_order 3 --sc_p_order 2 \
+#                                    --uncertain "all" \
+#                                    --mpi
 
 # MpiPoolSolver - MC
 #mpiexec -n 4 $python_cmd simple_example_testmodel.py \
@@ -50,11 +50,11 @@ mpiexec -n 4 $python_cmd simple_example_testmodel.py \
 #                                    --mpi
 
 # MpiSolverOld - SC
-mpiexec -n 4 $python_cmd simple_example_testmodel.py \
-                                    --model "testmodel" \
-                                    --uq_method "sc" --sc_q_order 3 --sc_p_order 2 \
-                                    --uncertain "all" \
-                                    --mpi --mpi_method "old"
+#mpiexec -n 4 $python_cmd simple_example_testmodel.py \
+#                                    --model "testmodel" \
+#                                    --uq_method "sc" --sc_q_order 3 --sc_p_order 2 \
+#                                    --uncertain "all" \
+#                                    --mpi --mpi_method "old"
 
 # MpiSolverOld - MC
 #mpiexec -n 4 $python_cmd simple_example_testmodel.py \
