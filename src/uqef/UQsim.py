b190f904d82856c553d0af5261ec00bdcc113c0f
Florian Künzner <florian.kuenzner@tum.de>
2019-06-18 16:15:37 +0200

    first structure refactoring for UQsim

---
+++ src/uqef/UQsim.py
@@ -0,0 +1,7 @@
+
+
+
+class UQsim(object):
+
+    def __init__(self):
+        pass
--- src/uqef/simulation/__init__.py
+++ src/uqef/simulation/__init__.py
@@ -7,4 +7,3 @@ Simulation package
 from .Simulation import Simulation
 from .McSimulation import McSimulation
 from .ScSimulation import ScSimulation
-from .Nodes import Nodes
